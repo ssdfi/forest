698d3e29f78a7246b1dffe0cfe98aabdbe45734c
Martin Dell Oro <martindell.oro@gmail.com>
2016-10-18 09:08:24 -0300

    Movimientos CRUD

--- src/AppBundle/Controller/MovimientosController.php
+++ src/AppBundle/Controller/MovimientosController.php
@@ -2,20 +2,139 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Movimientos;
+use AppBundle\Form\MovimientosType;
 
+/**
+ * Movimientos controller.
+ *
+ * @Route("/movimientos")
+ */
 class MovimientosController extends Controller
 {
     /**
-     * @Route("/", name="")
+     * Lists all Movimientos entities.
+     *
+     * @Route("/", name="movimientos_index")
+     * @Method("GET")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        $em = $this->getDoctrine()->getManager();
+
+        $movimientos = $em->getRepository('AppBundle:Movimientos')->findAll();
+
+        return $this->render('movimientos/index.html.twig', array(
+            'movimientos' => $movimientos,
+        ));
+    }
+
+    /**
+     * Creates a new Movimientos entity.
+     *
+     * @Route("/new", name="movimientos_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $movimiento = new Movimientos();
+        $form = $this->createForm('AppBundle\Form\MovimientosType', $movimiento);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($movimiento);
+            $em->flush();
+
+            return $this->redirectToRoute('movimientos_show', array('id' => $movimiento->getId()));
+        }
+
+        return $this->render('movimientos/new.html.twig', array(
+            'movimiento' => $movimiento,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Movimientos entity.
+     *
+     * @Route("/{id}", name="movimientos_show")
+     * @Method("GET")
+     */
+    public function showAction(Movimientos $movimiento)
+    {
+        $deleteForm = $this->createDeleteForm($movimiento);
+
+        return $this->render('movimientos/show.html.twig', array(
+            'movimiento' => $movimiento,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Movimientos entity.
+     *
+     * @Route("/{id}/edit", name="movimientos_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Movimientos $movimiento)
+    {
+        $deleteForm = $this->createDeleteForm($movimiento);
+        $editForm = $this->createForm('AppBundle\Form\MovimientosType', $movimiento);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($movimiento);
+            $em->flush();
+
+            return $this->redirectToRoute('movimientos_edit', array('id' => $movimiento->getId()));
+        }
+
+        return $this->render('movimientos/edit.html.twig', array(
+            'movimiento' => $movimiento,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Movimientos entity.
+     *
+     * @Route("/{id}", name="movimientos_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Movimientos $movimiento)
+    {
+        $form = $this->createDeleteForm($movimiento);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($movimiento);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('movimientos_index');
+    }
+
+    /**
+     * Creates a form to delete a Movimientos entity.
+     *
+     * @param Movimientos $movimiento The Movimientos entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Movimientos $movimiento)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('movimientos_delete', array('id' => $movimiento->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
     }
 }
---
+++ src/AppBundle/Form/MovimientosType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MovimientosType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('numeroFicha')
+            ->add('anioInspeccion')
+            ->add('fechaEntrada', 'date')
+            ->add('fechaSalida', 'date')
+            ->add('etapa')
+            ->add('estabilidadFiscal')
+            ->add('observacion')
+            ->add('observacionInterna')
+            ->add('auditar')
+            ->add('createdAt', 'datetime')
+            ->add('updatedAt', 'datetime')
+            ->add('destino')
+            ->add('expediente')
+            ->add('inspector')
+            ->add('reinspector')
+            ->add('responsable')
+            ->add('validador')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Movimientos'
+        ));
+    }
+}
