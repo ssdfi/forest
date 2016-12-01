8691b8508a1dc3ad011d7073c5768be612b8143e
Martin Dell Oro <martindell.oro@gmail.com>
2016-12-01 08:43:05 -0300

    Controlador de expedientes

--- src/AppBundle/Controller/ExpedientesController.php
+++ src/AppBundle/Controller/ExpedientesController.php
@@ -191,33 +191,6 @@ class ExpedientesController extends Controller
         ;
     }
 
-    /**
-     * Finds and displays a Movimientos entity.
-     *
-     * @Route("/expedientes/{id}/movimientos/new", name="nuevo_movimiento")
-     * @Method("GET")
-     */
-    public function newMovimientos(Request $request,$id)
-    {
-        $movimiento = new MovimientosController();
-        $movimiento->setContainer($this->container);
-        return $movimiento->newAction($request, $id);
-
-    }
-
-    /**
-     * Create new Movimientos entity.
-     *
-     * @Route("/expedientes/{id}/movimientos/{idMov}", name="list_movimientos")
-     * @Method("GET")
-     */
-    public function listMovimientos(Request $request,$id, $idMov)
-    {
-        $movimiento = new MovimientosController();
-        $movimiento->setContainer($this->container);
-        return $movimiento->indexAction($id, $idMov);
-
-    }
 
     /**
      * Finds and displays a Actividades entity.
