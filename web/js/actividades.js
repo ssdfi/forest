3dcd8e3af78451668fe4f9ed6b35b23d04b4b340
Martin Dell' Oro <martindell.oro@gmail.com>
2017-05-26 12:36:45 -0300

    Javascript de actividades finalizado

--- web/js/actividades.js
+++ web/js/actividades.js
@@ -1,13 +1,15 @@
 function ActividadPlantacionAdd() {
     var collectionHolder = $('.actividadesPlantaciones');
-    // alert(JSON.stringify(collectionHolder));
     var collectionCount = collectionHolder.children().length;
     var prototipo = collectionHolder.attr('data-prototype');
     prototipo = prototipo.replace(/__name__/g, collectionCount);
     collectionCount++;
-
     var newLi = jQuery('<tr class="fields"></tr>').html(prototipo);
     newLi.appendTo(collectionHolder);
-    // form = prototype.replace(/\$\$name\$\$/g, collectionHolder.children().length);
-    // collectionHolder.append(form);
+}
+
+function ActividadPlantacionRemove(btn) {
+  var row = $(btn).closest('tr');
+  row.remove();
+  return false;
 }
