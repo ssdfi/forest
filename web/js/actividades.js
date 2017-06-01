10e4560cf06d00ce9985522cd4f0c230052501fa
Martin Dell' Oro <martindell.oro@gmail.com>
2017-06-01 10:21:12 -0300

    Get Hectarea Function

--- web/js/actividades.js
+++ web/js/actividades.js
@@ -9,7 +9,29 @@ function ActividadPlantacionAdd() {
 }
 
 function ActividadPlantacionRemove(btn) {
-  var row = $(btn).closest('tr');
+  var row = $(btn).closest('.fields');
   row.remove();
   return false;
+}
+
+function getHectarea(btn){
+  var button, input, plantacion_id, tr;
+  button = $(btn);
+  tr = button.closest('tr');
+  input = tr.find('[superficie_registrada]');
+  plantacion_id = tr.find('[id$=plantacion]').val();
+  if (plantacion_id) {
+        input.prop('disabled', true);
+        button.prop('disabled', true);
+        return $.ajax({
+          url: "/plantaciones/json/" + plantacion_id
+        }).done(function(data) {
+          if (data) {
+            return tr.find('[id$=superficieRegistrada]').val(data['1']);
+          }
+        }).always(function() {
+          input.prop('disabled', false);
+          return button.prop('disabled', false);
+        });
+      }
 }
