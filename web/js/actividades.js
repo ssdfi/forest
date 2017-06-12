1baee555118ae1258e2be4ee2cec139a78281c22
Martin Dell' Oro <martindell.oro@gmail.com>
2017-06-12 10:28:38 -0300

    Actividades Javascript

--- web/js/actividades.js
+++ web/js/actividades.js
@@ -1,37 +1,81 @@
-function ActividadPlantacionAdd() {
-    var collectionHolder = $('.actividadesPlantaciones');
-    var collectionCount = collectionHolder.children().length;
-    var prototipo = collectionHolder.attr('data-prototype');
-    prototipo = prototipo.replace(/__name__/g, collectionCount);
-    collectionCount++;
-    var newLi = jQuery('<tr class="fields"></tr>').html(prototipo);
-    newLi.appendTo(collectionHolder);
-}
+(function ($) {
+  $(document).ready(function () {
+    $('#add-plantacion').click(function (e) {
+      e.preventDefault();
+      ActividadPlantacionAdd();
+      return;
+    });
+    $('.remove-plantacion').click(function (e) {
+      e.preventDefault();
+      ActividadPlantacionRemove(this);
+      return;
+    });
+    $('.hectareas').click(function (e) {
+      e.preventDefault();
+      getHectarea(this);
+      return;
+    });
+    function ActividadPlantacionAdd() {
+        var collectionHolder = $('.actividadesPlantaciones');
+        var collectionCount = collectionHolder.children().length;
+        var prototipo = collectionHolder.attr('data-prototype');
+        prototipo = prototipo.replace(/__name__/g, collectionCount);
+        collectionCount++;
+        var newLi = jQuery('<tr class="fields"></tr>').html(prototipo);
+        newLi.appendTo(collectionHolder).trigger('create');
+        return;
+    }
 
-function ActividadPlantacionRemove(btn) {
-  var row = $(btn).closest('.fields');
-  row.remove();
-  return false;
-}
+    function ActividadPlantacionRemove(btn) {
+      var row = $(btn).closest('.fields');
+      row.remove();
+      return false;
+    }
 
-function getHectarea(btn){
-  var button, input, plantacion_id, tr;
-  button = $(btn);
-  tr = button.closest('tr');
-  input = tr.find('[superficie_registrada]');
-  plantacion_id = tr.find('[id$=plantacion]').val();
-  if (plantacion_id) {
-        input.prop('disabled', true);
-        button.prop('disabled', true);
-        return $.ajax({
-          url: "/plantaciones/json/" + plantacion_id
-        }).done(function(data) {
-          if (data) {
-            return tr.find('[id$=superficieRegistrada]').val(data['1']);
+    function getHectarea(btn){
+      var button, input, plantacion_id, tr;
+      button = $(btn);
+      tr = button.closest('tr');
+      input = tr.find('[superficie_registrada]');
+      plantacion_id = tr.find('[id$=plantacion]').val();
+      if (plantacion_id) {
+            input.prop('disabled', true);
+            button.prop('disabled', true);
+            return $.ajax({
+              url: "/plantaciones/json/" + plantacion_id
+            }).done(function(data) {
+              if (data) {
+                return tr.find('[id$=superficieRegistrada]').val(data['1']);
+              }
+            }).always(function() {
+              input.prop('disabled', false);
+              return button.prop('disabled', false);
+            });
           }
-        }).always(function() {
-          input.prop('disabled', false);
-          return button.prop('disabled', false);
+    }
+
+    $('#plantaciones-modal-add').click(function() {
+          var plantacion_id, tr, _i, _len, _ref;
+          _ref = $('#plantaciones-ids').val().split('\n');
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            plantacion_id = _ref[_i];
+            if ($.isNumeric($.trim(plantacion_id))) {
+              $("#add-plantacion").click();
+              tr = $('#plantaciones').find('tr').last();
+              tr.find('[id$=plantacion]').val($.trim(plantacion_id));
+              tr.find('[id$=fecha]').val($('#fecha').val());
+              tr.find('[id$=numero_plantas]').val($('#numero_plantas').val());
+              if ($('#superficie_registrada').val()) {
+                tr.find('[id$=superficie_registrada]').val($('#superficie_registrada').val());
+              } else {
+                tr.find('button.hectareas').click();
+              }
+              tr.find('[id$=estado_aprobacion_id]').val($('#_estado_aprobacion_id').val());
+              tr.find('[id$=observaciones]').val($('#observaciones').val());
+            }
+          }
+          $('#plantaciones-ids').val('');
+          return $("#plantaciones-modal").modal('hide');
         });
-      }
-}
+  });
+})(jQuery);
