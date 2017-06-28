fd7a4bd5f504c5e2a820228477a94c7622a70621
Martin Dell' Oro <martindell.oro@gmail.com>
2017-06-28 10:18:00 -0300

    Actividades Titulares

--- src/AppBundle/Form/ActividadesTitularesType.php
+++ src/AppBundle/Form/ActividadesTitularesType.php
@@ -5,17 +5,26 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormEvents;
 
 class ActividadesTitularesType extends AbstractType
 {
     /**
      * {@inheritdoc}
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder->add('superficiePresentada')->add('superficieCertificada')->add('superficieInspeccionada')->add('observaciones')->add('actividad')->add('titular')->add('tipoPlantacion')->add('especie')        ;
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+          //->add('titular')
+          ->add('tipoPlantacion',EntityType::class, array('class'=>'AppBundle\Entity\TiposPlantacion', 'placeholder' => "Seleccione una opción" ))
+          ->add('especie')
+          ->add('superficiePresentada')
+          ->add('superficieCertificada')
+          ->add('superficieInspeccionada')
+          ->add('observaciones',TextareaType::class,array('required'=>false,'attr' => array('class' => 'form-control')));
+
     }
-    
     /**
      * {@inheritdoc}
      */
