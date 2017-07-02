c89f1df59305e2f242b936df8786acd3a9507ac6
Martin Dell' Oro <martindell.oro@gmail.com>
2017-07-02 16:53:31 -0300

    Modifico AcitividadeTitulares

--- src/AppBundle/Form/ActividadesTitularesType.php
+++ src/AppBundle/Form/ActividadesTitularesType.php
@@ -8,21 +8,66 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormEvents;
+use AppBundle\Entity\Titulares;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Form\DataTransformer\TitularesToNumberTransformer;
 
 class ActividadesTitularesType extends AbstractType
 {
+      private $manager;
+
+      public function __construct(ObjectManager $manager) {
+         $this->manager = $manager;
+      }
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options) {
+
+      $transformer = new TitularesToNumberTransformer($this->manager);
         $builder
-          //->add('titular')
           ->add('tipoPlantacion',EntityType::class, array('class'=>'AppBundle\Entity\TiposPlantacion', 'placeholder' => "Seleccione una opción" ))
           ->add('especie')
           ->add('superficiePresentada')
           ->add('superficieCertificada')
           ->add('superficieInspeccionada')
           ->add('observaciones',TextareaType::class,array('required'=>false,'attr' => array('class' => 'form-control')));
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function(FormEvent $event){
+              $form=$event->getForm();
+              $data=$event->getData();
+              if($data->getTitular()){
+                $titular= $data->getTitular();
+              }else{
+                $titular = null;
+              }
+              $form->add('plantacion_titular_id', HiddenType::class, array(
+                            'data' => ($titular !== null) ? $titular->getId() : '',
+                            'mapped' => false,
+              ));
+              $form->add('titular', TextType::class, array(
+                            'data' => ($titular !== null) ? $titular->getNombre() : '',
+                            'required'=>true,
+                            'attr' => ['disabled' => 'disabled'],
+              ));
+          });
+
+        $builder->addEventListener(
+            FormEvents::PRE_SUBMIT,
+            function(FormEvent $event){
+              $form=$event->getForm();
+              $data=$event->getData();
+              $titular = $this->manager->getRepository('AppBundle:Titulares')->findOneById($data['plantacion_titular_id']);
+              $data['titular'] = $titular;
+              $event->setData($data);
+          });
 
     }
     /**
