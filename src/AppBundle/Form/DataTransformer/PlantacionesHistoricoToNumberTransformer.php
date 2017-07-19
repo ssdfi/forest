65ba4c523877885fc70a63dd0184e55784f3afdd
Martin Dell' Oro <martindell.oro@gmail.com>
2017-07-19 10:08:04 -0300

    Data transformer

---
+++ src/AppBundle/Form/DataTransformer/PlantacionesHistoricoToNumberTransformer.php
@@ -0,0 +1,68 @@
+<?php
+namespace AppBundle\Form\DataTransformer;
+
+use AppBundle\Entity\Titulares;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class PlantacionesHistoricoToNumberTransformer implements DataTransformerInterface
+{
+    private $manager;
+
+    public function __construct(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * Transforms an object (issue) to a string (number).
+     *
+     * @param  Plantaciones|null $issue
+     * @return string
+     */
+    public function transform($issue)
+    {
+        if (null === $issue) {
+            return null;
+        }
+        $choices = array();
+        foreach ($issue as $key => $value) {
+          $choices[] = $value->getPlantacionNueva()->getId();
+        }
+        return $choices;
+    }
+
+    /**
+     * Transforms a string (number) to an object (issue).
+     *
+     * @param  string $issueNumber
+     * @return Plantaciones|null
+     * @throws TransformationFailedException if object (issue) is not found.
+     */
+    public function reverseTransform($issueNumber)
+    {
+        // no issue number? It's optional, so that's ok
+        if (!$issueNumber) {
+            return;
+        }
+
+        $issue = $this->manager
+            ->getRepository('AppBundle:Plantaciones')
+            // query for the issue with this id
+            ->findById($issueNumber)
+        ;
+
+        if (null === $issue) {
+            // causes a validation error
+            // this message is not shown to the user
+            // see the invalid_message option
+            throw new TransformationFailedException(sprintf(
+                'An issue with number "%s" does not exist!',
+                $issueNumber
+            ));
+        }
+        //dump($issue[0]);
+        return $issue[0];
+    }
+}
