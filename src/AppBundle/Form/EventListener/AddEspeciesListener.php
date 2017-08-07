352c65a2640a135fe5906a50ee7ce83fa7a7aef5
Martin Dell' Oro <martindell.oro@gmail.com>
2017-08-07 13:50:49 -0300

    Event listener to especies and historico

---
+++ src/AppBundle/Form/EventListener/AddEspeciesListener.php
@@ -0,0 +1,51 @@
+<?php
+// src/AppBundle/Form/EventListener/AddEmailFieldListener.php
+namespace AppBundle\Form\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class AddEspeciesListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FormEvents::PRE_SET_DATA => 'onPreSetData',
+            FormEvents::PRE_SUBMIT   => 'onPreSubmit',
+        );
+    }
+
+    public function onPreSetData(FormEvent $event)
+    {
+        $data = $event->getData();
+        $form = $event->getForm();
+
+        // Check whether the user from the initial data has chosen to
+        // display his email or not.
+        if (true === $data->getEspecie()) {
+            $form->add('especie');
+        }
+    }
+
+    public function onPreSubmit(FormEvent $event)
+    {
+        $data = $event->getData();
+        $form = $event->getForm();
+
+        if (!$data) {
+            return;
+        }
+
+        // Check whether the user has chosen to display his email or not.
+        // If the data was submitted previously, the additional value that
+        // is included in the request variables needs to be removed.
+        if (array_key_exists('especie', $data)) {
+            $form->add('especie');
+        } else {
+            unset($data['especie']);
+            $event->setData($data);
+        }
+    }
+}
---
+++ src/AppBundle/Form/EventListener/AddHistoricoListener.php
@@ -0,0 +1,48 @@
+<?php
+// src/AppBundle/Form/EventListener/AddEspeciesListener.php
+namespace AppBundle\Form\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class AddHistoricoListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FormEvents::PRE_SET_DATA => 'onPreSetData',
+            FormEvents::PRE_SUBMIT   => 'onPreSubmit',
+        );
+    }
+
+    public function onPreSetData(FormEvent $event)
+    {
+        $data = $event->getData();
+        $form = $event->getForm();
+        // dump($data);
+        // dump($data->getEspecie());
+        // if (true === $user->isShowEmail()) {
+        //     $form->add('email', EmailType::class);
+        // }
+    }
+
+    public function onPreSubmit(FormEvent $event)
+    {
+        $data = $event->getData();
+        $form = $event->getForm();
+        // $id_plantacion = $form->getViewData()->getId();
+
+        if (!$data) {
+            return;
+        }
+
+        if (array_key_exists('historico', $data) && true == $data['historico']) {
+            $form->add('historico');
+        } else {
+            unset($data['historico']);
+            $event->setData($data);
+        }
+    }
+}
