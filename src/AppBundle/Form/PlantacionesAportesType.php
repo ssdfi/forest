d985b9d1bbbdacd8280d7a2611aecacb0a25fdfc
Martin Dell' Oro <martindell.oro@gmail.com>
2017-09-19 10:46:30 -0300

    PLantaciones APortes type

--- src/AppBundle/Form/PlantacionesAportesType.php
+++ src/AppBundle/Form/PlantacionesAportesType.php
@@ -13,9 +13,41 @@ class PlantacionesAportesType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('idOrig')->add('anioPlantacion')->add('nomenclaturaCatastral')->add('distanciaPlantas')->add('cantidadFilas')->add('distanciaFilas')->add('densidad')->add('anioInformacion')->add('fechaImagen')->add('activo')->add('comentarios')->add('mpfId')->add('unificadoId')->add('createdAt')->add('updatedAt')->add('geom')->add('baseGeometricaId')->add('usuario')->add('accion')->add('modificacion')->add('objetivoPlantacion')->add('fuenteInformacion')->add('provincia')->add('departamento')->add('usoAnterior')->add('error')->add('fuenteImagen')->add('estratoDesarrollo')->add('tipoPlantacion')->add('usoForestal')->add('estadoPlantacion')->add('titular')        ;
+        $builder->add('idOrig')
+        ->add('anioPlantacion')
+        ->add('nomenclaturaCatastral')
+        ->add('distanciaPlantas')
+        ->add('cantidadFilas')
+        ->add('distanciaFilas')
+        ->add('densidad')
+        ->add('anioInformacion')
+        ->add('fechaImagen')
+        ->add('activo')
+        ->add('comentarios')
+        ->add('mpfId')
+        ->add('unificadoId')
+        ->add('createdAt')
+        ->add('updatedAt')
+        ->add('geom')
+        ->add('baseGeometricaId')
+        ->add('usuario')
+        ->add('accion')
+        ->add('modificacion')
+        ->add('objetivoPlantacion')
+        ->add('fuenteInformacion')
+        ->add('provincia')
+        ->add('departamento')
+        ->add('usoAnterior')
+        ->add('error')
+        ->add('fuenteImagen')
+        ->add('estratoDesarrollo')
+        ->add('tipoPlantacion')
+        ->add('usoForestal')
+        ->add('estadoPlantacion')
+        ->add('titular')        ;
+
     }
-    
+
     /**
      * {@inheritdoc}
      */
