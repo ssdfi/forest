10f2ebcaedeb4aab098fe15087ca879113328d66
Martin Dell Oro <martindell.oro@gmail.com>
2018-03-19 22:31:28 -0300

    Elimino linea

--- src/AppBundle/Form/PlantacionesEditarType.php
+++ src/AppBundle/Form/PlantacionesEditarType.php
@@ -88,7 +88,6 @@ class PlantacionesEditarType extends AbstractType
             ->add('objetivoPlantacion', EntityType::class, array('class'=>'AppBundle\Entity\ObjetivosPlantacion', 'placeholder' => "Seleccione una opción" ,'required'=>false))
             ->add('activo', CheckboxType::class, array('attr' => array('data-label' => 'Activo'), 'label' => false, 'required'=>false))
             ->add('comentarios');
-
         $builder->addEventSubscriber(new AddEspeciesListener())->addModelTransformer($transformer);
 
         $builder->addEventListener(FormEvents::PRE_SET_DATA,
