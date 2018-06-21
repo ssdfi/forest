da2f99eef32e3dee0af911e7ca1efeafb3340b30
Martin Dell Oro <martindell.oro@gmail.com>
2018-06-21 09:17:42 -0300

    Actividades

--- src/AppBundle/Entity/Actividades.php
+++ src/AppBundle/Entity/Actividades.php
@@ -85,7 +85,7 @@ class Actividades
     private $tipoActividad;
 
     /**
-        * @ORM\OneToMany(targetEntity="ActividadesPlantaciones",mappedBy="actividad",fetch="LAZY")
+        * @ORM\OneToMany(targetEntity="ActividadesPlantaciones",mappedBy="actividad",fetch="LAZY",cascade={"persist"})
         * @ORM\JoinTable(
         *      name="actividades_plantaciones",
         *      joinColumns={@ORM\JoinColumn(name="actividad_id", referencedColumnName="id")},
